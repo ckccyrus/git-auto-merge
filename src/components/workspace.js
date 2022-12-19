785b24fab8f0ce988955141b45673841ead18724
214 TONY <214@tranthinktech.com>
2022-12-19 14:44:14 +0800

    dev: add stdout for shelljs exec result

--- src/components/workspace.js
+++ src/components/workspace.js
@@ -82,21 +82,21 @@ class Workspace{
         shelljs.exec(`git pull`);
 
         console.log(`Source Branch Author`);
-        _returnObj.sourceBranchAuthor = shelljs.exec(`git log -1 --pretty=format:'%an'`);
+        _returnObj.sourceBranchAuthor = shelljs.exec(`git log -1 --pretty=format:'%an'`).stdout;
         console.log(`\n`);
 
         console.log(`Source Branch Commit Hash`);
-        _returnObj.sourceBranchCommitHash = shelljs.exec(`git log -n 1 --pretty=format:'%H'`);
+        _returnObj.sourceBranchCommitHash = shelljs.exec(`git log -n 1 --pretty=format:'%H'`).stdout;
         console.log(`\n`);
 
         console.log(`Source Branch Commit Message`);
-        _returnObj.sourceBranchCommitMsg = shelljs.exec(`git log --oneline --pretty=format:'(%an) %s' --no-merges --max-count=1 ${_returnObj.sourceBranchCommitHash}`);
+        _returnObj.sourceBranchCommitMsg = shelljs.exec(`git log --oneline --pretty=format:'(%an) %s' --no-merges --max-count=1 ${_returnObj.sourceBranchCommitHash}`).stdout;
         console.log(`\n`);
         console.log(`==================================================`);
 
 
         try{
-            let _result = shelljs.exec(`git merge origin/${$sourceBranch} -m "[ci-skip] Auto merge branch ${$sourceBranch} into ${$destinationBranch}"`),
+            let _result = shelljs.exec(`git merge origin/${$sourceBranch} -m "[ci-skip] Auto merge branch ${$sourceBranch} into ${$destinationBranch}"`).stdout,
                 _status = _result.code,
                 _isSuccess = _status == 0;
 
